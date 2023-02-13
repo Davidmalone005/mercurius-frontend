3704a1b182d6c7a413c3f810077ad54da8be1cd2
Onimisea <onimisea@gmail.com>
2023-02-13 17:53:04 +0100

    Insert the window and user checker into its own useEffect

--- pages/index.js
+++ pages/index.js
@@ -27,13 +27,15 @@ export default function Home({
 
   const [userStatus, setUserStatus] = useState(null);
 
-  if (typeof window !== "undefined" || typeof window !== null) {
-    if (session && session.user && session.user.name) {
-      window.localStorage.setItem("UserData", JSON.stringify(session.user));
-      setUserInfo(session.user);
-      setUserStatus(session.user);
+  useEffect(() => {
+    if (typeof window !== "undefined" || typeof window !== null) {
+      if (session && session.user && session.user.name) {
+        window.localStorage.setItem("UserData", JSON.stringify(session.user));
+        setUserInfo(session.user);
+        setUserStatus(session.user);
+      }
     }
-  }
+  }, []);
 
   useEffect(() => {
     if (products.length !== 0) {
